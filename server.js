30e573007e65e0296a3c1334f34730c04a1bd23a
RaFl91 <Terrell.Flemings@gmail.com>
2018-11-15 15:25:53 -0500

    Stopped following Bog app/ begin following Idea-Board and Project 2

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 require('dotenv').config()
-const express = require('express')
 const mongoose = require('mongoose')
-const logger = require('morgan')
-const app = express()
-
 mongoose.connect(process.env.MONGODB_URI)
 
-const db = mongoose.connection
-db.on('error', err => {
-  console.log(err)
-})
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-db.on('open', () => {
-  console.log('Connected to MongoDB')
-})
+var indexRouter = require('./routes/index')
+
+var app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 app.use(logger('dev'))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.send("Hello World")
-})
+app.use('/', indexRouter)
 
-const PORT = process.env.PORT || 3001
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+});
 
-app.listen(PORT, () => {
-  console.log('App is up and running on port ' + PORT)
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+});
+
+module.exports = app
