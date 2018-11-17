1e5e6c64916a112ea08f63e2459dd6b844df9515
RaFl91 <Terrell.Flemings@gmail.com>
2018-11-16 22:03:00 -0500

    route test successful

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const characterController = require('../controllers/characterController')
 
 // ===== USER ROUTES =====
 router.get('/api/users', userController.index)
-router.post('/api/users/', userController.create)
+router.post('/api/users', userController.create)
 router.get('/api/users/:userId', userController.show)
 router.patch('/api/users/:userId', userController.update)
 router.delete('/api/users/:userId', userController.delete)
@@ -18,3 +18,5 @@ router.get('/api/character/:characterId', characterController.show)
 router.delete('/api/characters/:characterId', characterController.delete)
 router.patch('/api/characters/:characterId', characterController.update)
 router.post('/api/users/:userId/characters', characterController.create)
+
+module.exports = router
