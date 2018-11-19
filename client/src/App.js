e896612c30c415f89f4aa842388996e9be1442d7
RaFl91 <Terrell.Flemings@gmail.com>
2018-11-19 10:00:29 -0500

    Styling until I figure out components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import NavBar from './components/NavBar';
+import HomePage from './components/HomePage'
+import NavBar from './components/NavBar'
+import User from './components/User'
 class App extends Component {
   render() {
     return (
       <Router>
       <div>
         <NavBar />
-        <h1>Hello from App</h1>
+        <Switch>
+          <Route exact path="/users/:userId" component={User}/>
+          <Route path='/' component={HomePage}/>
+        </Switch>
       </div>
       </Router>
     );
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+
+//  ===== Styling =====
+const NavBarStyles = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: right;
+    background: #242D42;
+    height: 15vh;
+    a {
+        text-decoration: none;
+        color: #241959;
+        &:hover{
+           color: #E6ED96; 
+        }
+    }
+
+`
 export default class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">Constructor</Link>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <NavBarStyles>
+                <Link to="/">Constructor</Link>
+            </NavBarStyles>
+        )
+    }
 }
